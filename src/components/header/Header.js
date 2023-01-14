a629372b6ef46c995f173932de1235f0c7287f60
vealruk <vladimir.kurlaev.97@gmail.com>
2023-01-14 19:07:03 +0700

    Finish base framework functional

---
+++ src/components/header/Header.js
@@ -0,0 +1,34 @@
+import {
+  ExcelComponent
+} from '@core/ExcelComponent';
+
+export class Header extends ExcelComponent {
+  static className = 'excel__header'
+
+  // constructor($root) {
+  //   super($root, {
+  //     name: 'Header',
+  //     listeners: ['input']
+  //   })
+  // }
+
+  toHTML() {
+    return `
+    <input class="input" type="text" value="Новая таблица" />
+
+    <div>
+
+      <div class="button">
+        <i class="material-icons">
+          delete
+        </i>
+      </div>
+      <div class="button">
+        <i class="material-icons">
+          exit_to_app
+        </i>
+      </div>
+
+    </div>`
+  }
+}
